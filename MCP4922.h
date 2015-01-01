72c92dfc79c82928ef49c4cc1b99e7fba550479c
helgenodland <helgen78@hotmail.com>
2015-01-01 18:06:26 +0100

    Cleaned up

--- MCP4922.h
+++ MCP4922.h
@@ -1,24 +1,14 @@
-/*
-  MCP4922.cpp - Library for driving MCP4922 2 channel DAC using hardware SPI.
-  Created by Helge Nodland, January 1, 2015.
-  Released into the public domain.
-*/
 
+/******************************************************************************
+*  Device:   MCP4922                                                          *
+*  Platform: Arduino                                                          *
+*  File:     MCP4922.h                                                        *
+*                                                                             *
+*  MCP4922.cpp - Library for driving MCP4922 2 channel DAC using hardware SPI.*
+*  Created by Helge Nodland, January 1, 2015.                                 *
+*  Released into the public domain.                                           *
+******************************************************************************/
 
-/*************************************************************************
-**  Device: MCP4922                                                 	**
-**  File:   AH_MCP4922.h - Library for get out analog voltage          	**
-**								    	**
-**  Created by A. Hinkel 2011-12-28                                 	**
-**  download from "http://www.alhin.de/arduino"  			**
-**									**
-**  Based on Code from:							**
-**  http://www.sinneb.net/2010/06/mcp4921-12bit-dac-voltage-controller/ **
-**									**
-**  Released into the public domain.  		                    	**
-**                                                                  	**
-**                                                                  	**
-*************************************************************************/
 
 
 #ifndef MCP4922_h
@@ -50,13 +40,3 @@ class MCP4922
 };
 
 #endif
-
-/******************************** 
-*				*
-* DAC=LOW  => DAC_A select	*
-* DAC=HIGH => DAC_B select	*
-*				*
-* GAIN=LOW  => 2x gain select	*
-* GAIN=HIGH => 1x gain select	*
-*				*
-********************************/
